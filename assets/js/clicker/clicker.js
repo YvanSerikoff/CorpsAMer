5af0a976a82e20d9a66d0f1bb84a98eefc85e0a6
Yvan <yvanserikoff@gmail.com>
2024-12-06 02:37:52 +0100

    image d'avancement

--- assets/js/clicker/clicker.js
+++ assets/js/clicker/clicker.js
@@ -1,49 +1,74 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    const plankton = document.getElementById('plankton');
-    const scoreDisplay = document.getElementById('score');
-    const totalScoreDisplay = document.getElementById('totalScore');
-    const progressBar = document.getElementById('progressBar');
+const plankton = document.getElementById('plankton');
 
-    let score = 500;
-    let totalScore = 0;
+const scoreDisplay = document.getElementById('score');
+const totalScoreDisplay = document.getElementById('totalScore');
+const scoreFinalDisplay = document.getElementById('scoreFinal')
 
-    // Retrieve score and totalScore from localStorage if they exist
-    const savedScore = localStorage.getItem('score');
-    const savedTotalScore = localStorage.getItem('totalScore');
-    if (savedScore !== null) {
-        score = parseInt(savedScore, 10); // Convert to number
-        scoreDisplay.textContent = score; // Update display
+const progressBar = document.getElementById('progressBar');
+const body = document.querySelector('body');
+const resetButton = document.getElementById('reset-button');
+
+const savedScore = localStorage.getItem('score');
+const savedTotalScore = localStorage.getItem('totalScore');
+const savedFinalScore = localStorage.getItem('scoreFinal');
+
+let score = 500;
+let totalScore = 0;
+let scoreFinal = 1000000;
+
+if (savedScore !== null) {
+    score = parseInt(savedScore, 10); // Convert to number
+    scoreDisplay.textContent = score; // Update display
+}
+
+if (savedTotalScore !== null) {
+    totalScore = parseInt(savedTotalScore, 10); // Convert to number
+    totalScoreDisplay.textContent = totalScore; // Update display
+    progressBar.value = totalScore; // Update progress bar
+}
+
+if(savedFinalScore !== null){
+    scoreFinal = parseInt(savedFinalScore, 10);
+}
+
+if (score >= scoreFinal) {
+    alert('Congratulations! You have reached the final score!');
+    // Additional logic for when the final score is reached
+}
+
+function changeBackground(bElement, bUrl) {
+    return bElement.style.backgroundImage = "url("+bUrl+")";
+}
+
+plankton.addEventListener('click', function() {
+    if (plankton.classList.contains('clicked')) {
+        plankton.classList.remove('clicked');
+        void plankton.offsetWidth; // Force reflow to restart the animation
     }
-    if (savedTotalScore !== null) {
-        totalScore = parseInt(savedTotalScore, 10); // Convert to number
-        totalScoreDisplay.textContent = totalScore; // Update display
-        progressBar.value = totalScore; // Update progress bar
-    }
+    score += 10000;
+    totalScore += 10000;
+    scoreDisplay.textContent = score;
+    totalScoreDisplay.textContent = totalScore;
+    progressBar.value = totalScore; // Update progress bar
+    localStorage.setItem('score', score);
+    localStorage.setItem('totalScore', totalScore);
 
-    plankton.addEventListener('click', function() {
-        if (plankton.classList.contains('clicked')) {
-            plankton.classList.remove('clicked');
-            void plankton.offsetWidth; // Force reflow to restart the animation
-        }
-        score += 10000;
-        totalScore += 10000;
-        scoreDisplay.textContent = score;
-        totalScoreDisplay.textContent = totalScore;
-        progressBar.value = totalScore; // Update progress bar
-        localStorage.setItem('score', score);
-        localStorage.setItem('totalScore', totalScore);
+    if (totalScore >= 20000) changeBackground(body, 'assets/images/clicker/bg2.jpg');
+    if (totalScore >= 30000) changeBackground(body, 'assets/images/clicker/bg3.jpg');
+    if (totalScore >= 40000) changeBackground(body, 'assets/images/clicker/bg4.jpg');
 
-        plankton.classList.add('clicked');
-    });
+    plankton.classList.add('clicked');
+});
 
-    const resetButton = document.getElementById('reset-button');
-    resetButton.addEventListener('click', () => {
-        score = 0; // Reset score
-        totalScore = 0; // Reset totalScore
-        scoreDisplay.textContent = score; // Update display
-        totalScoreDisplay.textContent = totalScore; // Update display
-        progressBar.value = totalScore; // Reset progress bar
-        localStorage.removeItem('score'); // Remove score from localStorage
-        localStorage.removeItem('totalScore'); // Remove totalScore from localStorage
-    });
+
+
+resetButton.addEventListener('click', () => {
+    score = 0; // Reset score
+    totalScore = 0; // Reset totalScore
+    scoreDisplay.textContent = score; // Update display
+    totalScoreDisplay.textContent = totalScore; // Update display
+    progressBar.value = totalScore; // Reset progress bar
+    localStorage.removeItem('score'); // Remove score from localStorage
+    localStorage.removeItem('totalScore'); // Remove totalScore from localStorage
+    body.style.backgroundImage = 'url(assets/images/clicker/bg1.jpg)'; // Reset to original background
 });
